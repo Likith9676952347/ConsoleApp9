b1755aeac9a90414da8f575133191bad15a0935b
Likith9676952347 <likithkumarreddy143@gmail.com>
2022-01-23 15:37:46 +0530

    Add project files.

---
+++ ConsoleApp9/Accountn.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp9
+{
+    class Accountn
+    {
+        public Accountn()
+        {
+            Console.WriteLine("Account Constructor");
+        }
+        public virtual void AccountName()
+        {
+            Console.WriteLine("LIKITH");
+        }
+    }
+    class Customer : Accountn
+    {
+        public override void AccountName()
+        {
+            Console.WriteLine("LUCIFER");
+        }
+    }
+}
+
